19c3523fb7cfbe589a567387e38df2e932559ba9
Zoey Joiz De Las Alas <arrazoe@gmail.com>
2023-11-02 10:39:08 -0700

    Created a scripting project for if statements
    Created a new scene for the script which determined whether the temperature of a coffee is too hot, cold, or just right

---
+++ ScriptingProjectP3DeLasAlas/Assets/Scripts/CoffeeTimer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeTimer : MonoBehaviour
+{
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+
+    void TemperatureTest ()
+    {
+        //If the coffee's temperature is greater than the hottest drinking temeperature
+        if (coffeeTemperature > hotLimitTemperature)
+        {
+            print("Coffee is too hot.");
+        }
+        else if (coffeeTemperature < coldLimitTemperature)
+        {
+            print("Coffee is too cold.");
+        }
+        else
+        {
+            print("Coffee is just right.");
+        }
+    }
+}
